2a7bfc2cd59b0240ae7f18897ac1917852b2f0c4
helouazizi <ouazizi2code@gmail.com>
2025-04-08 18:27:01 +0100

    add hesder

--- app/page.js
+++ app/page.js
@@ -1,19 +1,10 @@
-import Image from "next/image";
+import Header from "./components/header";
 
 export default function Home() {
   return (
     <div className="min-h-screen px-6 py-12 sm:px-20 font-sans bg-white text-black dark:bg-[#121212] dark:text-white">
-      <header>
-        <h1><a>Ouazizi</a></h1>
-        <div className="#">
-          <ul>
-            <li><a>About</a></li>
-            <li><a>Skills</a></li>
-            <li><a>Projects</a></li>
-            <li><a>Contact</a></li>
-          </ul>
-        </div>
-      </header>
+      {/* set up the header components */}
+      <Header/>
     </div>
   );
 }
