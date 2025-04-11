26ed81f49338858caaab8e08e5eca4371ed27f82
helouazizi <ouazizi2code@gmail.com>
2025-04-11 18:37:07 +0100

    add new setup

--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,16 @@
-// app/layout.js
-import '../styles/globals.css'; // Import global styles
-import Header from './components/header'; // Import Header component
-import Footer from './components/footer'; // Import Footer component
 
-export default function Layout({ children }) {
+import "../styles/globals.css" 
+export const metadata = {
+  title: "Ouazizi",
+  description: "Ouazizi a full-satck developper",
+};
+
+export default function RootLayout({ children }) {
   return (
-    <>
-      <html lang="en">
-        <body className="min-h-screen bg-white font-sans text-black dark:text-white">
-          {/* Header Component */}
-          <Header />
-
-          {/* Main Content */}
-          <main>{children}</main>
-
-          {/* Footer Component */}
-          <Footer />
-        </body>
-      </html>
-    </>
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
   );
 }
